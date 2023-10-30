4c3d048b7eb2c60ee7ffb1511a118b2151f6392a
JosethF <hjcabanillas@gps.local>
2023-10-30 20:13:26 +0100

    mucha cosa

--- random_cursor.py
+++ random_cursor.py
@@ -4,17 +4,17 @@ import random
 
 user32 = ctypes.windll.user32
 
-# Obtiene el tamaño de la pantalla
+# Get screen size
 width = user32.GetSystemMetrics(0)
 height = user32.GetSystemMetrics(1)
 
-# Mueve el cursor del mouse aleatoriamente
+# Move the cursor randomly
 while True:
     x = random.randint(0, width)
     y = random.randint(0, height)
     user32.SetCursorPos(x, y)
     time.sleep(0.5)
 
-    # Si se detecta un clic del botón izquierdo del mouse, se detiene el programa
+    # If it detects two clicks it will stop
     if user32.GetAsyncKeyState(0x01) != 0:
         break
--- time_sleeper.py
+++ time_sleeper.py
@@ -3,59 +3,49 @@ from datetime import datetime, timedelta
 import os
 import subprocess
 
+# Function to create the first window for user input
 def create_first_window():
+    # Create a Tkinter window
     first_window = Tk()
     first_window.title("Choose your desired time:")
-    first_window.geometry("400x230")
+    first_window.geometry("400x180")
 
-    separator = Frame(first_window, height=20, bg="")
-    separator.pack()
-
+    # GUI elements for user input
+    # Labels and entry fields for hours and minutes
     label = Label(first_window, text="At what time do you want?:")
     label.pack()
-
-    separator = Frame(first_window, height=5, bg="")
-    separator.pack()
-
     label = Label(first_window, text="Hours (24):")
     label.pack()
-
     entryH = Entry(first_window)
     entryH.pack()
-
-    separator = Frame(first_window, height=10, bg="")
-    separator.pack()
-
     label = Label(first_window, text="Minutes:")
     label.pack()
-
     entryM = Entry(first_window)
     entryM.pack()
 
-    separator = Frame(first_window, height=5, bg="")
-    separator.pack()
-
-    def run_sleeper(hours,mins):
+    # Function to set up the shutdown command and execute it
+    def run_sleeper(hours, mins):
+        # Get current time
         current_time = datetime.now().time()
-        input_time = datetime.strptime("{}:{}:00".format(hours,mins), "%H:%M:%S").time()
+        # Create target time based on user input
+        input_time = datetime.strptime("{}:{}:00".format(hours, mins), "%H:%M:%S").time()
+        # Calculate time difference between target and current time
         difference_time = timedelta(hours=input_time.hour - current_time.hour,
-                            minutes=input_time.minute - current_time.minute,
-                            seconds=input_time.second - current_time.second)
+                                    minutes=input_time.minute - current_time.minute,
+                                    seconds=input_time.second - current_time.second)
         totalSeconds = int(difference_time.total_seconds())
-
+        # Construct and execute shutdown command
         command = "shutdown -s -t {}".format(totalSeconds)
         subprocess.run(["shutdown", "/a"], stdout=subprocess.PIPE)
         subprocess.run(command, shell=True)
-
         first_window.destroy()
-
+        # If checkbox is checked, run random_cursor.exe
         if(let1.get() == 1):
-            path = os.path.join(os.getcwd(),"random_cursor.exe")
-            subprocess.run(path,shell=True)
-            subprocess.run(path,shell=True)
+            path = os.path.join(os.getcwd(), "random_cursor.exe")
+            subprocess.run(path, shell=True)
 
-
-    def check_time(hours,mins):
+    # Function to validate user input time
+    def check_time(hours, mins):
         current_hour = datetime.now().hour
         current_mins = datetime.now().minute
         if(current_hour == hours):
@@ -65,47 +55,40 @@ def create_first_window():
             return True
         return False
 
+    # Function to display a second window with a message
     def second_window(mssg):
         second_window = Tk()
         second_window.geometry("300x60")
-
-        separator = Frame(second_window, height=20, bg="")
-        separator.pack()
-
         label = Label(second_window, text=mssg)
         label.pack()
 
-        separator = Frame(second_window, height=20, bg="")
-        separator.pack()
-
+    # Function to handle the submission of user input
     def submit_function():
+        # Get user input for hours and minutes, validate it, and take appropriate action
         hours = -1 if entryH.get() == '' else int(entryH.get()) if entryH.get().isdigit() else -1
         minutes = -1 if entryM.get() == '' else int(entryM.get()) if entryM.get().isdigit() else -1
         
-        if( (hours>=0 and hours<24) and (minutes>=0 and minutes<60) ):
-            if(check_time(hours,minutes) == False):
-                second_window("Input time should be minor than current Time")
+        if( (hours >= 0 and hours < 24) and (minutes >= 0 and minutes < 60) ):
+            if(check_time(hours, minutes) == False):
+                second_window("Input time should be earlier than current time")
             else:
-                run_sleeper(hours,minutes)
+                run_sleeper(hours, minutes)
         else:
-            second_window("Wrong Time")
+            second_window("Invalid time input")
 
-    separator = Frame(first_window, height=10, bg="")
-    separator.pack()
-    
+    # GUI elements for checkbox and submit button
     let1 = IntVar()
     let1.set(1)
     c1 = Checkbutton(first_window, text='Run Random Cursor', variable=let1, onvalue=1, offvalue=0)
     c1.pack()
-
-    separator = Frame(first_window, height=10, bg="")
-    separator.pack()
-
     submit_button = Button(first_window, text="Submit", command=submit_function)
     submit_button.pack()
 
-    first_window.bind('<Return>',lambda event=None: submit_button.invoke())
+    # Bind Enter key to invoke the submit button
+    first_window.bind('<Return>', lambda event=None: submit_button.invoke())
 
+    # Start the main loop for the Tkinter window
     first_window.mainloop()
 
+# Call the function to create the first window
 create_first_window()
